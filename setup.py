ff7849b836761c1cd0b2d2df6999462662e77fb3
Manuel <miglesia@Manuels-MacBook-Pro.local>
2017-11-19 21:34:03 +0530

    save

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
         name  = "data_analysis",
         version = "0.0.1",
-        url = "www.github.com/iglesiasmanu/DAPY",
+        url = "www.github.com/iglesiasmanu/data_analysis",
         license = "BSD",
         author = "Manu",
         packages = find_packages(),
